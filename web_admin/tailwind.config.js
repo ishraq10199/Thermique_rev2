a49b38be423b941850d94fd6fa60b5cd78fcdbeb
Ishraq Hasan <ishraq10199@gmail.com>
2021-04-05 11:53:31 +0600

    Added web_admin

---
+++ web_admin/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        mullingar: ["Mullingar"],
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
